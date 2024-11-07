f4b493f4c8c4821f9ba0adc3d87566c9b47fcb09
Bernhard <beloureiro@gmail.com>
2024-11-07 09:07:04 +0000

    ajuste cards final

--- src/AppComponents/Solutions.js
+++ src/AppComponents/Solutions.js
@@ -38,7 +38,7 @@ const Solutions = ({ language }) => {
   // JSX to render the solutions
   return (
     <div
-      className="grid grid-cols-1 md:grid-cols-4 gap-6 mb-8 max-w-3xl mx-auto"
+      className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-8 max-w-3xl mx-auto"
       style={{ maxWidth: "850px" }} // Adjust the maximum width of the container
     >
       {solutions.map((solution, index) =>
@@ -49,34 +49,34 @@ const Solutions = ({ language }) => {
             href={solution.path}
             target="_blank"
             rel="noopener noreferrer"
-            className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300"
-            style={{ minHeight: "130px", width: "100%" }} // Adjust the minimum height and width of the solution card
+            className="bg-gray-800 p-4 rounded-lg hover:bg-gray-700 transition-colors duration-300"
+            // Set minHeight and maxHeight to control the height of the card
+            style={{ minHeight: "100px", maxHeight: "150px", width: "100%", padding: "10px" }}
           >
             <h3
-              className="text-xl font-bold mb-2"
+              className="text-lg font-bold mb-2"
               style={{ color: solution.titleColor }} // Adjust the title color
             >
               {solution.name}
             </h3>
-            <p className="text-gray-300">{solution.description}</p>{" "}
-            {/* Adjust the description text color */}
+            <p className="text-gray-300">{solution.description}</p>
           </a>
         ) : (
           // Internal link using React Router's Link component
           <Link
             key={index}
             to={solution.path}
-            className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300"
-            style={{ minHeight: "150px", width: "100%" }} // Adjust the minimum height and width of the solution card
+            className="bg-gray-800 p-4 rounded-lg hover:bg-gray-700 transition-colors duration-300"
+            // Set minHeight and maxHeight to control the height of the card
+            style={{ minHeight: "100px", maxHeight: "150px", width: "100%", padding: "10px" }}
           >
             <h3
-              className="text-xl font-bold mb-2"
+              className="text-lg font-bold mb-2"
               style={{ color: solution.titleColor }} // Adjust the title color
             >
               {solution.name}
             </h3>
-            <p className="text-gray-300">{solution.description}</p>{" "}
-            {/* Adjust the description text color */}
+            <p className="text-gray-300">{solution.description}</p>
           </Link>
         )
       )}
